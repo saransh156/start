f3036b22a9f388c62343a3a2cbb769ffceb6b76b
saransh156 <138297672+saransh156@users.noreply.github.com>
2024-10-23 22:06:17 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,1 +1,13 @@
 
+#include<stdio.h>
+int main(){
+int a,b,c;
+  printf("enter the side of a");
+    scanf("%d",&a);
+    printf("enter the side of b");
+    scanf("%d",&b);
+    printf("enter the side of c");
+    scanf("%d",&c);
+    printf("%d",a);
+ printf("%d",b);
+ printf("%d",c);
